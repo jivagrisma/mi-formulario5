6d564dd31453b931efde4b5743163b69a740eb91
Jorge Grisales <111651530+jivagrisma@users.noreply.github.com>
2024-06-01 15:24:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Agrega aquí tu lógica de validación o interacción con el formulario
+document.getElementById('registration-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Aquí puedes enviar los datos del formulario al servidor o realizar otras acciones
+});
